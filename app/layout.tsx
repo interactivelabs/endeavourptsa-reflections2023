f9e4e9aaf454c1378c59c616d9c46e93f7094105
Luis Gonzalez <luis.gonzalez@airbnb.com>
2023-09-22 17:25:36 -0700

    Added reflections banner and ptsa logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,14 +9,14 @@ export const metadata: Metadata = {
   description: 'Site for the Endeavour PTSA Reflections 2023 - 2024 program.',
 };
 
-export default function RootLayout({
-  children,
-}: {
+interface IRootLayout {
   children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
 }
+
+const RootLayout = ({ children }: IRootLayout) => (
+  <html lang="en" className="bg-slate-50">
+    <body className={inter.className}>{children}</body>
+  </html>
+);
+
+export default RootLayout;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      CONTENT
-    </main>
-  );
-}
+import Image from 'next/image';
+import ptsaLogo from '../public/images/EndeavourPTSALogo.jpg';
+import reflectionBanner from '../public/images/reflections-23-24.webp';
+
+const Home = () => (
+  <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <Image
+      src={reflectionBanner}
+      alt="Reflections program banner 2023 - 2024, I'm hopeful because..."
+    />
+    <Image src={ptsaLogo} alt="Endeavour Elementary PTSA logo" />
+  </main>
+);
+
+export default Home;
