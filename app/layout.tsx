1ebdc7701b706d8c6b99f39c611389f704a695e7
Luis Gonzalez <luis.gonzalez@airbnb.com>
2023-09-29 09:58:58 -0700

    Added new buttons and sutff

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,20 @@
+import clsx from 'clsx';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { DM_Sans, Inter } from 'next/font/google';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+});
+
+const dmSans = DM_Sans({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+  display: 'swap',
+  variable: '--font-dm-sans',
+});
 
 export const metadata: Metadata = {
   title: 'Reflections 2023 - 2024 | Endeavour PTSA',
@@ -14,8 +26,17 @@ interface IRootLayout {
 }
 
 const RootLayout = ({ children }: IRootLayout) => (
-  <html lang="en" className="bg-slate-50">
-    <body className={inter.className}>{children}</body>
+  <html
+    lang="en"
+    className={clsx(
+      'h-full bg-white antialiased',
+      inter.variable,
+      dmSans.variable,
+    )}
+  >
+    <body className="flex min-h-full">
+      <div className="flex w-full flex-col">{children}</div>
+    </body>
   </html>
 );
 
