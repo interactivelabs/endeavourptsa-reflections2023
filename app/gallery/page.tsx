0a1666bc7477b0b7908d64a2af214edbba32ae0b
Luis Gonzalez <luis.gonzalez@airbnb.com>
2023-11-03 14:52:26 -0700

    trying to get a deploy of the api fist

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -4,21 +4,22 @@ import ArtGallery from '@/components/sections/ArtGallery';
 import getServersUrl from '@/lib/getServerUrl';
 import { ListObjectsV2CommandOutput } from '@aws-sdk/client-s3';
 
-const getData = async () => {
-  const serverUrl = getServersUrl();
-  const response = await fetch(`${serverUrl}/api/gallery`);
-  const { Contents } = (await response.json()) as ListObjectsV2CommandOutput;
-  const items = Contents?.filter((item) => !!item.Size && item.Size > 0);
+// const getData = async () => {
+//   const serverUrl = getServersUrl();
+//   const response = await fetch(`${serverUrl}/api/gallery`);
+//   const { Contents } = (await response.json()) as ListObjectsV2CommandOutput;
+//   const items = Contents?.filter((item) => !!item.Size && item.Size > 0);
 
-  return items;
-};
+//   return items;
+// };
 
 const GalleryPage = async () => {
-  const data = await getData();
-  if (!data) return null;
+  // const data = await getData();
+  // if (!data) return null;
   return (
     <main className="p-8">
-      <ArtGallery images={data} />
+      {/* <ArtGallery images={data} /> */}
+      Gallery Page
     </main>
   );
 };
